6dbe6ea2925d48c669cc664f3783e8dd71d636d2
Ricardo <ricardo@Rick_S145.localdomain>
2021-10-11 11:30:30 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputCerveja = document.getElementById("cerveja");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("Calculando...");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let cerveja = inputCerveja.value;
+    let duracao = inputDuracao.value;
+
+    let totalCarne = carnePP(duracao) * adultos + (carnePP(duracao) /2 * criancas);
+    console.log(totalCarne);
+    let totalCerveja = cervejaPP(duracao) * cerveja 
+    let totalBebida = bebidaPP(duracao) * adultos + (bebidaPP(duracao) / 2 * criancas);
+
+    resultado.innerHTML = `<p>${totalCarne / 1000}Kg de carne</p>`
+    resultado.innerHTML += `<p>${totalCerveja / 1000}L de Cerveja</p>`
+    resultado.innerHTML += `<p>${totalBebida / 1000}L de Bebidas</p>`
+
+}
+
+function carnePP (duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP (duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidaPP (duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
